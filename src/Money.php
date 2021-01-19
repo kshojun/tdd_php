16e0f213483898acfc95f2e66c7b40d1c7985bd0
s-kimura <x.kimura.1@i3design.co.jp>
2021-01-19 11:04:53 +0900

    fix: Dollar, FrancオブジェクトをFactory Methodで生成

--- src/Money.php
+++ src/Money.php
@@ -11,4 +11,12 @@ class Money {
     function equals($object) {
         return $this->amount == $object->amount && get_class($object) == get_class($this);
     }
+
+    public static function dollar($amount) {
+        return new Dollar($amount);
+    }
+
+    public static function franc($amount) {
+        return new Franc($amount);
+    }
 }
