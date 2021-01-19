06a3050006fd026c6e013b60da7084cce6b1713b
s-kimura <x.kimura.1@i3design.co.jp>
2021-01-19 11:05:50 +0900

    fix: Creating objects with Factory Method

--- test/MoneyTest.php
+++ test/MoneyTest.php
@@ -3,22 +3,22 @@ require_once('vendor/autoload.php');
 
 class MoneyTest extends PHPUnit\Framework\TestCase {
     public function testMultiplication() {
-        $five = new money\Dollar(5);
-        $this->assertEquals(new money\Dollar(10), $five->times(2));
-        $this->assertEquals(new money\Dollar(15), $five->times(3));
+        $five = money\Money::dollar(5);
+        $this->assertEquals(money\Money::dollar(10), $five->times(2));
+        $this->assertEquals(money\Money::dollar(15), $five->times(3));
     }
 
     public function testEquality() {
-        $this->assertTrue((new money\Dollar(5))->equals(new money\Dollar(5)));
-        $this->assertFalse((new money\Dollar(5))->equals(new money\Dollar(6)));
-        $this->assertTrue((new money\Franc(5))->equals(new money\Franc(5)));
-        $this->assertFalse((new money\Franc(5))->equals(new money\Franc(6)));
-        $this->assertFalse((new money\Franc(5))->equals(new money\Dollar(5)));
+        $this->assertTrue((money\Money::dollar(5))->equals(money\Money::dollar(5)));
+        $this->assertFalse((money\Money::dollar(5))->equals(money\Money::dollar(6)));
+        $this->assertTrue((money\Money::franc(5))->equals(money\Money::franc(5)));
+        $this->assertFalse((money\Money::franc(5))->equals(money\Money::franc(6)));
+        $this->assertFalse((money\Money::franc(5))->equals(money\Money::dollar(5)));
     }
 
     public function testFrancMultiplication() {
-        $five = new money\Franc(5);
-        $this->assertEquals(new money\Franc(10), $five->times(2));
-        $this->assertEquals(new money\Franc(15), $five->times(3));
+        $five = money\Money::franc(5);
+        $this->assertEquals(money\Money::franc(10), $five->times(2));
+        $this->assertEquals(money\Money::franc(15), $five->times(3));
     }
 }
